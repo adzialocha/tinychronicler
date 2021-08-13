e81cae3321bd2ef340dc063084feeb592e61659f
Andreas Dzialocha <x12@adz.garden>
2021-08-13 12:45:13 +0200

    Add a little comment to helpers

--- tinychronicler/helpers.py
+++ tinychronicler/helpers.py
@@ -6,6 +6,9 @@ from contextlib import contextmanager
 
 @contextmanager
 def temporary_file(file_ext: str):
+    """
+    Prepares a temporary file which gets automatically cleaned up afterwards.
+    """
     tmp_dir = tempfile.mkdtemp()
     yield os.path.join(tmp_dir, "out{}".format(file_ext))
     shutil.rmtree(tmp_dir)
