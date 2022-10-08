6e87da91b9ead45ae4b0d4a4a913ffe5e24782ed
Andreas Dzialocha <x12@adz.garden>
2022-10-08 16:47:45 +0200

    Add a nice css transition

--- tinychronicler/web/src/views/Kiosk.tsx
+++ tinychronicler/web/src/views/Kiosk.tsx
@@ -1,10 +1,12 @@
-import { createElement, useEffect, useState, useMemo, useRef } from 'react';
+import React, { useEffect, useState, useMemo, useRef } from 'react';
 import styled from 'styled-components';
 import OSC from 'osc-js';
 
 const OSC_ENDPOINT = '127.0.0.1';
 const OSC_PORT = '8000/ws';
+
 const RECONNECTION_ATTEMPT_INTERVAL = 5000;
+const TRANSITION_DURATION = '100ms';
 
 type VisualState =
   | {
@@ -37,59 +39,148 @@ type AudioArgs = {
   url: string;
 };
 
-const StyledKiosk = styled.div`
+type Visible = {
+  visible: boolean;
+};
+
+const Container = styled.div`
   position: fixed;
   top: 0;
   right: 0;
   bottom: 0;
   left: 0;
-  background-color: #000;
-  z-index: 1000;
-  display: flex;
   justify-content: center;
   align-items: center;
   overflow: hidden;
+  z-index: 1000;
+  display: flex;
 `;
 
-const Image = styled.img`
+const StyledKiosk = styled(Container)`
+  background-color: #000;
+`;
+
+const Image = styled.img<Visible>`
   object-fit: contain;
   max-width: 100%;
   max-height: 100%;
   width: auto;
   height: auto;
+  opacity: ${(props) => (props.visible ? 1 : 0)};
+  transition: opacity ${TRANSITION_DURATION} linear;
 `;
 
-const Video = styled.video`
+const Video = styled.video<Visible>`
   object-fit: contain;
   max-width: 100%;
   max-height: 100%;
   width: auto;
   height: auto;
+  opacity: ${(props) => (props.visible ? 1 : 0)};
+  transition: opacity ${TRANSITION_DURATION} linear;
 `;
 
 const Audio = styled.audio`
   display: none;
 `;
 
-const ImageView = (props: ImageArgs) => {
-  return <Image src={props.url} />;
-};
+const ImageView = React.forwardRef<HTMLImageElement, Visible>((props, ref) => {
+  return <Image visible={props.visible} ref={ref} />;
+});
 
-const VideoPlayer = (props: VideoArgs) => {
-  return <Video autoPlay muted={props.muted} loop src={props.url} />;
-};
+const VideoPlayer = React.forwardRef<HTMLVideoElement, Visible>(
+  (props, ref) => {
+    return <Video visible={props.visible} ref={ref} loop />;
+  },
+);
 
-const AudioPlayer = (props: AudioArgs) => {
-  return <Audio autoPlay src={props.url} />;
-};
+const AudioPlayer = React.forwardRef<HTMLAudioElement>((_, ref) => {
+  return <Audio ref={ref} autoPlay />;
+});
 
 const Kiosk = () => {
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const imageRef = useRef<HTMLImageElement>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
   const [visualState, setVisualState] = useState<VisualState>({
     mode: 'black',
   });
   const [audioState, setAudioState] = useState<AudioState>({
     url: undefined,
   });
+
+  const startVideo = (args: VideoArgs) => {
+    if (!videoRef.current) {
+      return;
+    }
+
+    const video = videoRef.current;
+
+    // Reset source first when changing video, otherwise the old video will be
+    // visible during the fading css transition
+    if (video.src !== args.url) {
+      video.src = '';
+    }
+
+    video.src = args.url;
+    video.currentTime = args.seek;
+    video.muted = args.muted;
+    video.play();
+  };
+
+  const stopVideo = () => {
+    if (!videoRef.current) {
+      return;
+    }
+
+    const video = videoRef.current;
+    video.pause();
+  };
+
+  const showImage = (args: ImageArgs) => {
+    if (!imageRef.current) {
+      return;
+    }
+
+    const image = imageRef.current;
+
+    // Reset source first when changing image, otherwise the old image will be
+    // visible during the fading css transition
+    if (image.src !== args.url) {
+      image.src = '';
+    }
+
+    image.src = args.url;
+  };
+
+  const hideImage = () => {
+    if (!imageRef.current) {
+      return;
+    }
+
+    // Do nothing
+  };
+
+  const startAudio = (args: AudioArgs) => {
+    if (!audioRef.current) {
+      return;
+    }
+
+    const audio = audioRef.current;
+    audio.src = args.url;
+    audio.currentTime = 0;
+    audio.play();
+  };
+
+  const stopAudio = () => {
+    if (!audioRef.current) {
+      return;
+    }
+
+    const audio = audioRef.current;
+    audio.pause();
+  };
 
   const osc = useMemo(() => {
     return new OSC();
@@ -212,11 +303,38 @@ const Kiosk = () => {
     };
   }, [osc]);
 
+  useEffect(() => {
+    if (visualState.mode === 'video') {
+      hideImage();
+      startVideo(visualState);
+    } else if (visualState.mode === 'image') {
+      stopVideo();
+      showImage(visualState);
+    } else if (visualState.mode === 'black') {
+      hideImage();
+      stopVideo();
+    }
+  }, [visualState]);
+
+  useEffect(() => {
+    if (audioState.url) {
+      startAudio({
+        url: audioState.url,
+      });
+    } else {
+      stopAudio();
+    }
+  }, [audioState]);
+
   return (
     <StyledKiosk>
-      {audioState.url && <AudioPlayer url={audioState.url} />}
-      {visualState.mode === 'video' && <VideoPlayer {...visualState} />}
-      {visualState.mode === 'image' && <ImageView {...visualState} />}
+      <AudioPlayer ref={audioRef} />
+      <Container>
+        <VideoPlayer ref={videoRef} visible={visualState.mode === 'video'} />
+      </Container>
+      <Container>
+        <ImageView ref={imageRef} visible={visualState.mode === 'image'} />
+      </Container>
     </StyledKiosk>
   );
 };
